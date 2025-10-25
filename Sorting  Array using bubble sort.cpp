b358e4068ec00eb5c5985d05d5aa666e12970518
Amir Hossain Limon <146566739+Amirhossainlimon@users.noreply.github.com>
2025-10-25 22:57:06 +0600

    Update Sorting  Array using bubble sort.cpp

--- Sorting  Array using bubble sort.cpp
+++ Sorting  Array using bubble sort.cpp
@@ -3,28 +3,28 @@ using namespace std;
 
 int main() {
     int n;
+    cout<<"Enter size of an array"<<endl;
     cin >> n;
     int arr[n];
+ for(int i=0; i<n;i++){
+    cin>>arr[i];
+}
 
-    for(int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
-
-   int counter=1;
-         while(counter<n){
-        for(int i = 0; i < n -counter; i++) {
-            if(arr[i] > arr[i + 1]) {
-                swap(arr[i], arr[i + 1]);
+  for(int i=0; i<n-1;i++){
+    for(int j=0;j<n-i-1; j++){
+            if(arr[j]>arr[j+1]){
+        swap(arr[j],arr[j+1]);
             }
-        }
-        counter++;
     }
+}
 
-    for(int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
+  cout<<"After sorting array:";
+  for(int i=0; i<n;i++){
+    cout<<arr[i]<<" ";
+}
+
 
     return 0;
 }
+
 
